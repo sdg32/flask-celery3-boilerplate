a3a8359d1b3b25853b979da077693d289b1766dd
sdg32 <sdg32@outlook.com>
2017-06-07 12:42:32 +0800

    Move celery configurations to config file.

--- app/__init__.py
+++ app/__init__.py
@@ -13,19 +13,11 @@ from config import Config
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(Config)
-app.config.setdefault('BROKER_URL', 'redis://localhost:6379/0')
-app.config.setdefault('CELERYBEAT_SCHEDULER',
-                      'app.schedule.schedulers.DatabaseScheduler')
-app.config.setdefault('CELERY_RESULT_BACKEND',
-                      'app.schedule.backends.DatabaseBackend')
-app.config.setdefault('CELERY_SEND_EVENTS', True)
-app.config.setdefault('CELERY_SEND_TASK_SENT_EVENT', True)
 
 db = SQLAlchemy(app)
 celery = Celery('proj')
 celery.config_from_object(app.config)
 celery_state = celery.events.State()
-_celery_thread = None
 
 
 def run_celery_state_monitor():
--- config.py
+++ config.py
@@ -6,11 +6,23 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    # Database
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(
         os.path.join(base_dir, 'db_dev.sqlite3')
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_COMMIT_ON_TEARDOWN = False
 
+    # Celery
+    BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+    CELERYBEAT_SCHEDULER = 'app.schedule.schedulers.DatabaseScheduler'
+    CELERY_RESULT_BACKEND = 'app.schedule.backends.DatabaseBackend'
+    CELERY_SEND_EVENTS = True
+    CELERY_SEND_TASK_SENT_EVENT = True
+    CELERY_ACCEPT_CONTENT = os.getenv('CELERY_ACCEPT_CONTENT', ['json'])
+    CELERY_TASK_SERIALIZER = os.getenv('CELERY_TASK_SERIALIZER', 'json')
+    CELERY_EVENT_SERIALIZER = os.getenv('CELERY_EVENT_SERIALIZER', 'json')
+
+    # Flask
     DEBUG = True
     TESTING = False
